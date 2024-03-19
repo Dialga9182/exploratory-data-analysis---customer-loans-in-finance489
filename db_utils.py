93d8709f42e858bd6762b1e67651768a360a61cc
Dialga9182 <85892628+Dialga9182@users.noreply.github.com>
2024-03-19 17:08:56 +0000

    Update 19-03-2024-17:08
    Added things

--- db_utils.py
+++ db_utils.py
@@ -51,7 +51,7 @@ class RDSDatabaseConnector:
         data from the location where it is stored,
         and then saves that as a pandas dataframe.
     save_dataframe_to_csv(self)
-        Saves that dataframe to a csv file.
+        Saves that pandas dataframe to a csv file.
     load_dataframe_from_csv(self)
         Loads that dataframe from a csv file.
     '''
@@ -107,9 +107,111 @@ class RDSDatabaseConnector:
             The dataframe in question.
         '''
         df = pd.read_csv('data.csv')
-        print(df.shape)
+        column_names = []
+        for i in df:
+            column_names.append(i)
+        print(column_names)
+        return df
 
-if __name__=='__main__':
+class DataTransform:
+    '''
+    This class holds methods which, when called, will
+    transform the formats of certain columns in a table.
+    PARAMETERS
+    ATTRIBUTES
+    METHODS
+    '''
+    def __init__(self, df = pd.read_csv('data.csv')):
+        self.df = df
+        pass
+    def excess_symbol_removal(self):
+        '''
+        When called, will remove any symbols which
+        are not deemed necessary for our EDA goals
+        Must accept arguments which go on to
+        specify which symbols i.e., columns are
+        to be removed.
+        '''
+        symbols = str(input('separate by spaces which columns are to be dropped: ')).split()
+        print('These are the symbols that will be dropped: ',symbols)
+        self.df = self.df.drop(labels=symbols, axis=1, )
+        remaining_symbols = []
+        for i in self.df:
+            remaining_symbols.append(i)
+        print('These are the remaining symbols: ', remaining_symbols)
+        return self.df
+    def numerical_to_categorical(self):
+        '''
+        When called, will convert column values
+        from a numerical form to a categorical form.
+        Depending on the data involved, there could
+        be many categories. For instance:
+        1,2,3, to One, Two, Three.
+        '''
+                
+        self.df['term'] = self.df['term'].astype("category")
+        self.df['grade'] = self.df['grade'].astype("category") #made into category type!
+        #TODO: Grade was not originally a numerical, so one might want to consider changing the method name
+        #to something more like 'general_to_categorical' or something similar. Also change description too.
+        self.df['sub_grade'] = self.df['sub_grade'].astype("category")
+        self.df['home_ownership'] = self.df['home_ownership'].astype("category")
+        self.df['verification_status'] = self.df['verification_status'].astype("category")
+        self.df['loan_status'] = self.df['loan_status'].astype("category")
+        self.df['payment_plan'] = self.df['payment_plan'].astype("category")
+        self.df['purpose'] = self.df['purpose'].astype("category")
+        self.df['delinq_2yrs'] = self.df['delinq_2yrs'].astype("category")
+        self.df['application_type'] = self.df['application_type'].astype("category")
+        #TODO: This ^^^^^ is tedious and it reuses code. Automate it.
+        
+        
+        pass
+    def numerical_to_boolean():
+        '''
+        Goes from a numerical to a boolean pandas type
+        '''
+        
+        pass
+    def categorical_to_boolean():
+        pass
+    def int_to_float():
+        pass
+    def float_to_int():
+        pass
+    def convert_dates_to_proper_format():
+        pass
+    def method8():
+        pass
+    def method9():
+        pass
+    def add_more_methods_as_necessary():
+        pass
+    
+class DataFrameInfo:
+    '''DOCSTRING'''
+    def __init__(self):
+        pass
+    def method1_describe_all_columns_to_check_their_datatypes():
+        pass
+    def method2_extract_statistical_values_median_stddev_mean_from_cols_and_dataframe():
+        pass
+    def method3_count_distinct_values_in_categorical_columns():
+        pass
+    def method4_print_out_the_shape_of_the_dataframe():
+        pass
+    def method5_generate_a_count_slash_percentage_count_of_NULL_values_in_each_column():
+        pass
+    def method6_anyothermethodsyoufinduseful():
+        pass
+    '''
+    Describe all columns in the DataFrame to check their data types
+Extract statistical values: median, standard deviation and mean from the columns and the DataFrame
+Count distinct values in categorical columns
+Print out the shape of the DataFrame
+Generate a count/percentage count of NULL values in each column
+Any other methods you may find useful'''
+    pass
+
+if __name__ == '__main__':
     RDS = RDSDatabaseConnector()
     a = RDS.initialise_engine_and_extract_data()
     b = RDS.save_dataframe_to_csv()
