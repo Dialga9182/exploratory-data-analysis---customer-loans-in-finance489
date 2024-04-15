7befe8d2d69a8748e59cbec49bc149e82dd035db
Dialga9182 <85892628+Dialga9182@users.noreply.github.com>
2024-04-15 16:22:35 +0100

    Update functions.py
    Adds type hints to functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import yaml
+import pandas as pd
 
 def function_to_load_credentials():
-    """Create a python object from a YAML file.
-    Creates a python object from a YAML file.
-    
+    """Reads and returns credentials from YAML file.
+        
     Parameters:
     -----------
     credentials: python object
@@ -11,10 +11,10 @@ def function_to_load_credentials():
         in authorising access to remote database information.
     """
     with open("credentials.yaml", "r") as f:
-        credentials = yaml.safe_load(f)
+        credentials: dict = yaml.safe_load(f)
     return credentials
 
-def amount_of_nulls_and_column_drop(df):
+def amount_of_nulls_and_column_drop(df: pd.core.frame.DataFrame):
     """Print out the column names and the percentage of non-nulls in that column.
     This function prints out the column names
     and the percentage of non-nulls in that column.
@@ -23,9 +23,9 @@ def amount_of_nulls_and_column_drop(df):
     -----------
     """
     for element in df:
-        non_null_count = df[element].count()
-        total_count = df.shape[0]
-        percentage_non_null = (non_null_count / total_count) * 100
+        non_null_count: pd.Series = df[element].count()
+        total_count: pd.Series = df.shape[0]
+        percentage_non_null: float = (non_null_count / total_count) * 100
         print(element, ': Non-nulls:', non_null_count, 'Non-Null Percentage:', percentage_non_null, '%')
         if percentage_non_null < 80: # If non-nulls < 80% of total dataset, drop column
             print(f"Dropping column '{element}' with non-null percentage {percentage_non_null:.2f}%")
@@ -33,4 +33,5 @@ def amount_of_nulls_and_column_drop(df):
     return df
 
 if __name__ == '__main__':
+    print(type(function_to_load_credentials()))
     pass
